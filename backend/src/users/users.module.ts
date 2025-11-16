6a2b02594083eee36dcf53ee271956287981c5d0
rmoris <raja.moris@student.unimelb.edu.au>
2025-11-17 01:18:04 +1100

    [Feature] Setup user service

---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common"
+import { UserService } from "./users.service"
+
+@Module({
+    providers: [UserService],
+    exports: [UserService]
+})
+
+
+export class UsersModule { }
